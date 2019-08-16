7325d4be8b4deac015192e4d66677ee85e75976b
Mino Kim <51058295+ucvb@users.noreply.github.com>
2019-08-16 11:05:01 -0700

    v1.1
    Clean up

--- dailydevnull.sh
+++ dailydevnull.sh
@@ -6,9 +6,6 @@ monthday=`date +%d`
 year=`date +%Y`
 user=`whoami`
 
-# -n required character count to stop reading,
-# -p doesnt show what you're typing..?
-
 clear
 echo "Hi $user. It's $day, $month $monthday, $year."
 read -n 1 -r -p "Press any key to start writing."
@@ -30,62 +27,3 @@ done
 clear
 read -n 1 -r -p "Enjoy your day."
 clear
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#DAILY DEV NULL FUNCTION
-# input=${input%?};
-	# echo $input;
-	# sleep 0.01
-	# clear
-
-
-
-	# ATTEMPTS TO MAKE DELETION FASTER AS INPUT GETS LONGER
-	# wc=`echo $input | wc -m`
-		# if [ $wc -lt 10 ]; then
-		# sleep 0.02
-		# echo "lt10"
-		# elif [[ $wc -lt 30 ]]; then
-		# sleep 0.01
-		# echo "lt30"
-		# elif [[ $wc -lt 50 ]]; then
-		# echo "much faster!"
-		# echo
-		# else
-		# echo "SUPER FAST"
-		# fi
-
-# echo $wc
-
-# if [ $wc -lt 10 ]; then
-# 	echo "testing"
-# elif [[ $wc -lt 30 ]]; then
-# 	echo "little faster"
-# elif [[ $wc -lt 50 ]]; then
-# 	echo "much faster!"
-# else
-# 	echo "SUPER FAST"
-# fi
